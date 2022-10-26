a7ac44722c3192610b9925541d5a5a4e0b3aad7c
Alex Sun <alexsun82@icloud.com>
2022-10-26 22:40:36 +0200

    Implement package-lint suggestions

--- no-clown-fiesta-theme.el
+++ no-clown-fiesta-theme.el
@@ -1,31 +1,14 @@
-;;; no-clown-fiesta-theme.el --- Color theme for Emacs 24+. -*- lexical-binding: t -*-
+;;; no-clown-fiesta-theme.el --- Not-so-colorful-theme -*- lexical-binding: t -*-
 
-;; Permission is hereby granted, free of charge, to any person
-;; obtaining a copy of this software and associated documentation
-;; files (the "Software"), to deal in the Software without
-;; restriction, including without limitation the rights to use, copy,
-;; modify, merge, publish, distribute, sublicense, and/or sell copies
-;; of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-
-;; The above copyright notice and this permission notice shall be
-;; included in all copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
-
-;; Package-Requires: ((autothemer "0.2"))
+;; URL: https://github.com/ranmaru22/no-clown-fiesta-theme.el
+;; Author: Alex Sun (ranmaru22)
+;; Package-Version: 1.0
+;; Package-Requires: ((emacs "24.1") (autothemer "0.2"))
 
 ;;; Commentary:
 ;; Color theme for Emacs 24+ that does not look like a unicorn farted on your
 ;; screen.  Based on no-clown-fiesta.nvim by aktersnurra, converted and extended
-;; for Emacs by Alex Sun (ranmaru22).
+;; for Emacs by ranmaru22.
 
 ;; Original nvim theme: https://github.com/aktersnurra/no-clown-fiesta.nvim
 
