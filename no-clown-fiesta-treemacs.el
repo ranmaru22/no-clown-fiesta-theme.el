d5c453988ce8e1467d92b467bbe088d8648f8292
Alex Sun <alexsun82@icloud.com>
2023-02-19 16:45:58 +0100

    Add some comments to the treemacs theme file

--- no-clown-fiesta-treemacs.el
+++ no-clown-fiesta-treemacs.el
@@ -1,23 +1,6 @@
 ;;; no-clown-fiesta-treemacs.el --- Treemacs theme that goes along with no-clown-fiesta-theme -*- lexical-binding: t; no-byte-compile: t; -*-
 
-;; Copyright (c) 2022-2023 ranmaru22
-;;
-;; This program is free software: you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free Software
-;; Foundation, either version 3 of the License, or any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but WITHOUT
-;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-;; details.
-;;
-;; You should have received a copy of the GNU General Public License along with
-;; this program. If not, see http://www.gnu.org/licenses/.
-
 ;;; Commentary:
-;; You can enable the treemacs theme by calling the function
-;; `no-clown-fiesta-theme-treemacs' after loading the theme. Requires
-;; `all-the-icons' to display the file and folder icons.
 
 ;;; Code:
 (defun no-clown-fiesta-treemacs--line-space-hook ()
